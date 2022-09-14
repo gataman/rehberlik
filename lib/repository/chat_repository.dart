7627ff98c26701afa46412588bf509b691f22fb4
Gürcan ATAMAN <gurcanataman@gmail.com>
2022-09-14 18:08:39 +0300

    14.09.2022

---
+++ lib/repository/chat_repository.dart
@@ -0,0 +1,26 @@
+import '../common/locator.dart';
+import '../models/school.dart';
+import '../services/schools_service.dart';
+
+class SchoolRepository implements SchoolService {
+  final SchoolService _service = locator<SchoolService>();
+
+  @override
+  Future<String> add({required School object}) => _service.add(object: object);
+
+  @override
+  Future<void> delete({required String objectID}) => _service.delete(objectID: objectID);
+
+  @override
+  Future<void> update({required School object}) => _service.update(object: object);
+
+  @override
+  Future<List<School>> getAll({Map<String, dynamic>? filters}) => _service.getAll(filters: filters);
+
+  @override
+  Future<void> addAll({required List<School> list}) => _service.addAll(list: list);
+
+  @override
+  Future<int> getStudentCount({required String schoolID, required int classLevel, Map<String, dynamic>? filters}) =>
+      _service.getStudentCount(schoolID: schoolID, classLevel: classLevel);
+}
